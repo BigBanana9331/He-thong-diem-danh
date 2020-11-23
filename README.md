4c02586d7e13048dc3733fc3b18c73476b730aae
BigBanana9331 <BigBanana9331@users.noreply.github.com>
2020-11-23 15:40:17 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# faceRecognition
+He thong diem Danh
+
+# B1: Train Model
+```
+    #: python ./trainSave.py
+```
+
+Ta duoc model train.pkl
+
+# B2: Tren Jetson Nano chay:
+```
+    #: python ./faceRecognition-jetson.py
+```
+
+Neu tren Windows chay:
+```
+    #: python ./faceRecognition-windows.py
+```
+
+# Stack
+MySQL
+OpenCV2
---
+++ seed.py
@@ -0,0 +1,17 @@
+import mysql.connector
+import constant
+
+mydb = mysql.connector.connect(
+    host=constant.dbInfor["host"],
+	user=constant.dbInfor["user"],
+	password=constant.dbInfor["password"],
+	database=constant.dbInfor["database"]
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE TABLE IF NOT EXISTS students (id int(11) NOT NULL AUTO_INCREMENT, name VARCHAR(255) NOT NULL UNIQUE, username VARCHAR(255) NOT NULL UNIQUE, status bool default 0, PRIMARY KEY (id))")
+mycursor.execute("insert into students (name, username) values ('Bao Huy', 'huy')")
+mycursor.execute("insert into students (name, username) values ('Phuc Thinh', 'tonyngu')")
+
+mydb.commit()
